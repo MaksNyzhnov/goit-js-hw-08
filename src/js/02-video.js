9aeca173c5006255a0461f962165c12107878543
MaksNyzhnov <123084582+MaksNyzhnov@users.noreply.github.com>
2023-06-05 23:54:21 +0300

    fixed errors in video and form

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -3,15 +3,19 @@ import Player from '@vimeo/player';
 
 const iframeEL = document.querySelector('iframe');
     const player = new Player(iframeEL);
-
+const STORAGE_KEY = "videoplayer-current-time";
 
 player.on("timeupdate", throttle(timeSavehandler, 1000));
 
-player.setCurrentTime(JSON.parse(localStorage.getItem("videoplayer-current-time")))
+if (localStorage.getItem(STORAGE_KEY)) {
+player.setCurrentTime(JSON.parse(localStorage.getItem(STORAGE_KEY)))
+}
+
 
 function timeSavehandler(data) {
     
     const timeData = JSON.stringify(Math.round(data.seconds))
-    localStorage.setItem("videoplayer-current-time", timeData);
+    localStorage.setItem(STORAGE_KEY, timeData);
     
 }
+
--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -7,17 +7,21 @@ const inputMassageEl = document.querySelector('textarea')
 const submitBtnEl = document.querySelector('button')
 const formData = {}
 
+const STORAGE_KEY = "feedback-form-state";
 
 
 formEl.addEventListener('input', throttle(onFormInput, 500))
 formEl.addEventListener('submit', onFormSubmit)
+
+
+
 
 function onFormInput(event) {
     if (event.target.nodeName !== "INPUT" && event.target.nodeName !== "TEXTAREA") {
     return
     }
     formData[event.target.name] = event.target.value;
-    localStorage.setItem("feedback-form-state", JSON.stringify(formData))
+    localStorage.setItem(STORAGE_KEY, JSON.stringify(formData))
 
     
 }
@@ -28,14 +32,16 @@ function onFormSubmit(event) {
     const email = inputEmailEl.value;
     const message = inputMassageEl.value;
     console.log(formData)
-    localStorage.removeItem("feedback-form-state")
+    localStorage.removeItem(STORAGE_KEY)
     event.target.reset()
 
 }
 
+
+
 try {
-    const savedMail = JSON.parse(localStorage.getItem("feedback-form-state")).email;
-    const savedMessage = JSON.parse(localStorage.getItem("feedback-form-state")).message;
+    const savedMail = JSON.parse(localStorage.getItem(STORAGE_KEY)).email;
+    const savedMessage = JSON.parse(localStorage.getItem(STORAGE_KEY)).message;
     
     inputEmailEl.value = savedMail;
 inputMassageEl.value = savedMessage;
@@ -43,4 +49,11 @@ inputMassageEl.value = savedMessage;
 
 }
 
+if (!JSON.parse(localStorage.getItem(STORAGE_KEY)).hasOwnProperty("email")) {
+    inputEmailEl.value = "";
+}
+
+if (!JSON.parse(localStorage.getItem(STORAGE_KEY)).hasOwnProperty("message")) {
+    inputMassageEl.value = "";
+}
 
